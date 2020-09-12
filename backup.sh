0a5bf4b265a07aa94ee7d2fb44956a2d0aa020bf
chadgeary <chad.geary@gmail.com>
2020-09-12 02:38:23 +0000

    server_backup for rm

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 BACKUP_DIR='/opt/minecraft/backups'
 
 # Remove old backup(s)
-find $BACKUP_DIR -type -f -mtime +4 -name 'backup*.tar.gz' -delete
+find $BACKUP_DIR -type -f -mtime +4 -name 'server_backup*.tar.gz' -delete
 
 # Archive current server state
 tar cvf - /opt/minecraft/server | gzip -9 > $BACKUP_DIR/server_backup_$(date +"%F_%H_%M_%S").tar.gz
